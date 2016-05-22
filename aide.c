9c75a336ce393868a3e727da4fa6c788f8238a82
kalilinuxfrance <kali.linux.france@gmail.com>
2016-05-22 16:50:01 +0200

    man page

--- aide.c
+++ aide.c
@@ -3,10 +3,9 @@
 
 void MenuAide()
 {
-
-
-printf("Domotics-duino v0.1(https://github.com/kalilinuxfrance/Domotics-duino)\n");
-printf("Usage: domoticsduino [Impulsion Type] [Adresse GPIO]\n");
+printf("Domotics-duino v0.1 by Julien Amrein(https://github.com/kalilinuxfrance/Domotics-duino)\n");
+printf("GNU General Public License.\n\n");
+printf("Usage:\tdomoticsduino [Impulsion Type] [Adresse GPIO]\n");
 printf("\nIMPULSION TYPE\n");
 printf("\t0 : Pas de tension aux bornes du gpio\n");
 printf("\t1 : Tension aux bornes du gpio\n");
@@ -22,10 +21,10 @@ printf("\t6\t-Exterieur EST\n");
 printf("\t7\t-Exterieur SUD\n");
 printf("\t8\t-Exterieur OUEST\n");
 printf("\t9\t-Exterieur NORD\n");
-printf("\t10\n");
-printf("\t11\n");
-printf("\t12\n");
-printf("\t13\n");
+printf("\t10\t-N/A\n");
+printf("\t11\t-N/A\n");
+printf("\t12\t-N/A\n");
+printf("\t13\t-N/A\n");
 printf("\nEXAMPLES:\n");
-printf("\tdomoticsduino 2 3 : Cree une impulsion pour enclencher le telerupteur de la cuisine\n");
+printf("\n\tdomoticsduino 2 3 : Cree une impulsion pour enclencher le telerupteur de la cuisine\n");
 }
